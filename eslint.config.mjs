7d58de6d1ea7557d572ec68264c346de0e78af69
Kreliannn <krelianquimson@gmail.com>
2025-01-27 20:29:33 +0800

    fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    env: { development: true },
+    rules: {
+      "no-console": "warn", // Only show warnings for console.logs
+    },
+  },
 ];
 
 export default eslintConfig;
+
