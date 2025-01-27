1f6a4960bddf932833f67f999fdf078b5fd868da
Kreliannn <krelianquimson@gmail.com>
2025-01-27 17:04:36 +0800

    add landing page animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Button, Container, Typography } from "@mui/material"
 import CodeIcon from "@mui/icons-material/Code"
 import InstallDesktopIcon from "@mui/icons-material/InstallDesktop"
@@ -7,35 +8,78 @@ import FeedbackIcon from "@mui/icons-material/Feedback"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import Link from "next/link"
+import gsap from "gsap"
+import { useRef, useEffect } from "react"
 
 export default function LandingPage() {
 
+  let pic = useRef(null)
+  let header1 = useRef(null)
+  let header2 = useRef(null)
+  let header3 = useRef(null)
+  let button = useRef(null)
+  useEffect(() => {
+    const timeline = gsap.timeline();
+    
+    gsap.fromTo(
+      pic.current,
+      { x: 50, opacity: 0 },
+      { x: -0, opacity: 1, duration: 1 }
+    )
 
+    timeline
+      .fromTo(
+        header1.current,
+        { x: -50, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1 },
+        "-=0.3" // Starts slightly before the previous animation ends
+      )
+      .fromTo(
+        header2.current,
+        { x: -50, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1 },
+        "-=0.3" // Starts slightly before the previous animation ends
+      )
+      .fromTo(
+        header3.current,
+        { x: -50, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1.5 },
+        "-=0.3" // Starts slightly before the previous animation ends
+      )
+      .fromTo(
+        button.current,
+        { x: -50, opacity: 0 },
+        { x: 0, opacity: 1, duration: 1.5 },
+        "-=1" // Starts earlier to sync with header3
+      );
+  }, []);
+  
 
   return (
     <Container maxWidth="lg" className="py-12">
     <section className="flex flex-col md:flex-row items-center justify-between mb-16">
         <div className="md:w-1/2 mb-8 md:mb-0">
-          <h1  className="text-5xl font-bold mb-4">
+          <h1 ref={header1} className="text-5xl font-bold mb-4 opacity-0">
             Dev Starter
           </h1>
-          <h1  className="text-2xl text-gray-600 mb-4">
+          <h1 ref={header2} className="text-2xl text-gray-600 mb-4 opacity-0">
             Simplify Your Development Setup
           </h1>
-          <h1  className="text-gray-800 mb-6 text-md">
+          <h1 ref={header3} className="text-gray-800 mb-6 text-md opacity-0">
             Dev Starter is your one-stop solution for kickstarting development projects. We provide easy-to-use tools
             and resources to streamline your setup process, allowing you to focus on what matters most - writing great
             code.
           </h1>
-          <button  className="button">
+          <button ref={button} className="button opacity-0">
             <Link href="/pages/commandGenerator" > Get Started </Link>
           </button>
         </div>
         <div className="md:w-1/2 flex justify-end">
           <img
+            ref={pic}
             src="/pic.jpg"
             alt="Dev Starter Illustration"
-            className="rounded-lg shadow-lg w-96 h-96"
+            className="rounded-lg shadow-lg w-96 h-96 opacity-0"
           />
         </div>
       </section>
