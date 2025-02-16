39665760fb7f2b4dc79eaf357cdacb8638420533
Kreliannn <krelianquimson@gmail.com>
2025-02-16 18:06:15 +0800

    add typescript  field tin abckend

--- src/app/pages/commandGenerator/components/backend.tsx
+++ src/app/pages/commandGenerator/components/backend.tsx
@@ -4,19 +4,19 @@ import useCommandStore from "@/app/store/commandStore";
 
 
 const backend = [
-  { name : "express", command : "express @types/express", img : "https://img.icons8.com/color/512/express-js.png", documentation : "https://devdocs.io/express-getting-started/" },
-  { name : "mongoose", command : "mongoose @types/mongoose", img : "https://4008838.fs1.hubspotusercontent-na1.net/hubfs/4008838/mogoose-logo.png", documentation : "https://mongoosejs.com/docs/" },
-  { name : "passport", command : "passport @types/passport passport-local @types/passport-local", img : "https://cdn.glitch.me/project-avatar/0d184ee3-fd8d-4b94-acf4-b4e686e57375.png", documentation : "https://www.passportjs.org/docs/" },
-  { name : "cors", command : "cors", img : "https://images.velog.io/images/leejh96/post/c613f6bd-7ed5-45a7-9c12-f3bcfaa0ec50/227652.png", documentation : "https://www.npmjs.com/package/cors" },
-  { name : "socket io", command : "socket.io @types/socket.io", img : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/2048px-Socket-io.svg.png", documentation : "https://socket.io/docs/v4/" },
-  { name : "gemeni", command : "@google/generative-ai", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr7qrIazsvZwJuw-uZCtLzIjaAyVW_ZrlEQ&s", documentation : "https://ai.google.dev/gemini-api/docs" },
-  { name : "open ai", command : "openai @types/openai", img : "https://cdn.worldvectorlogo.com/logos/openai-2.svg", documentation : "https://platform.openai.com/docs/quickstart" },
-  { name : "cookie parser", command : "cookie-parser @types/cookie-parser", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIjCGnxYtwOu0H5owyaoSvbof60Y4OQt0vuQ&s", documentation : "https://expressjs.com/id/resources/middleware/cookie-parser.html" },
-  { name : "express session", command : "express-session @types/express-session", img : "https://logovectordl.com/wp-content/uploads/2020/10/session-technologies-usesession-com-logo-vector.png", documentation : "https://www.npmjs.com/package/express-session" },
-  { name : "express validator", command : "express-validator", img : "https://images.opencollective.com/express-validator/36a8af1/logo/256.png", documentation : "https://express-validator.github.io/docs/" },
-  { name : "doteEnv", command : "dotenv", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHaNT3Fi8RMNUpPDk-Zddeo2FTvDN3Sye5AA&s", documentation : "https://www.npmjs.com/package/dotenv" },
-  { name : "JWT", command : "jsonwebtoken @types/jsonwebtoken", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Pte0L4XLvNqEWXbkCXk_LDN6eSpcaDDLhQ&s", documentation : "https://www.geeksforgeeks.org/jwt-authentication-with-node-js/" },
-  { name : "multer", command : "multer @types/multer", img : "https://miro.medium.com/v2/resize:fit:1400/1*QTZvtnHWanNxBQBynhtlIA.png", documentation : "https://www.npmjs.com/package/multer" },
+  { name : "express", command : "express", typescript : "@types/express", img : "https://img.icons8.com/color/512/express-js.png", documentation : "https://devdocs.io/express-getting-started/" },
+  { name : "mongoose", command : "mongoose", typescript : "@types/mongoose", img : "https://4008838.fs1.hubspotusercontent-na1.net/hubfs/4008838/mogoose-logo.png", documentation : "https://mongoosejs.com/docs/" },
+  { name : "passport", command : "passport passport-local", typescript : "@types/passport @types/passport-local", img : "https://cdn.glitch.me/project-avatar/0d184ee3-fd8d-4b94-acf4-b4e686e57375.png", documentation : "https://www.passportjs.org/docs/" },
+  { name : "cors", command : "cors", typescript : "", img : "https://images.velog.io/images/leejh96/post/c613f6bd-7ed5-45a7-9c12-f3bcfaa0ec50/227652.png", documentation : "https://www.npmjs.com/package/cors" },
+  { name : "socket io", command : "socket.io", typescript : "@types/socket.io", img : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Socket-io.svg/2048px-Socket-io.svg.png", documentation : "https://socket.io/docs/v4/" },
+  { name : "gemeni", command : "@google/generative-ai", typescript : "", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThr7qrIazsvZwJuw-uZCtLzIjaAyVW_ZrlEQ&s", documentation : "https://ai.google.dev/gemini-api/docs" },
+  { name : "open ai", command : "openai", typescript : "@types/openai", img : "https://cdn.worldvectorlogo.com/logos/openai-2.svg", documentation : "https://platform.openai.com/docs/quickstart" },
+  { name : "cookie parser", command : "cookie-parser", typescript : "@types/cookie-parser", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIjCGnxYtwOu0H5owyaoSvbof60Y4OQt0vuQ&s", documentation : "https://expressjs.com/id/resources/middleware/cookie-parser.html" },
+  { name : "express session", command : "express-session", typescript : "@types/express-session", img : "https://logovectordl.com/wp-content/uploads/2020/10/session-technologies-usesession-com-logo-vector.png", documentation : "https://www.npmjs.com/package/express-session" },
+  { name : "express validator", command : "express-validator", typescript : "", img : "https://images.opencollective.com/express-validator/36a8af1/logo/256.png", documentation : "https://express-validator.github.io/docs/" },
+  { name : "doteEnv", command : "dotenv", typescript : "", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHaNT3Fi8RMNUpPDk-Zddeo2FTvDN3Sye5AA&s", documentation : "https://www.npmjs.com/package/dotenv" },
+  { name : "JWT", command : "jsonwebtoken", typescript : "@types/jsonwebtoken", img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5Pte0L4XLvNqEWXbkCXk_LDN6eSpcaDDLhQ&s", documentation : "https://www.geeksforgeeks.org/jwt-authentication-with-node-js/" },
+  { name : "multer", command : "multer", typescript : "@types/multer", img : "https://miro.medium.com/v2/resize:fit:1400/1*QTZvtnHWanNxBQBynhtlIA.png", documentation : "https://www.npmjs.com/package/multer" },
 ]
 
 
