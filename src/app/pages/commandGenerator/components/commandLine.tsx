1ff4a9d557d29068e84c34d020d8ba1840c9881f
Kreliannn <krelianquimson@gmail.com>
2025-02-16 20:28:36 +0800

    enhance the ui of toggle button between js and ts

--- src/app/pages/commandGenerator/components/commandLine.tsx
+++ src/app/pages/commandGenerator/components/commandLine.tsx
@@ -6,7 +6,7 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import CheckIcon from '@mui/icons-material/Check';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-
+import Image from 'next/image';
 
 export default function CommandLine({ toggle ,setToggle } : { toggle : boolean, setToggle :React.Dispatch<React.SetStateAction<boolean>> } )
 {
@@ -37,23 +37,38 @@ export default function CommandLine({ toggle ,setToggle } : { toggle : boolean, 
 
     return(
         <div className='w-11/12  drop-shadow-lg fixed rounded overflow-hidden  ' style={{bottom : "3%", left: "3%"}}>
-            <div className="container-fluid bg-black flex justify-end h-12 ">
+            <div className="container-fluid bg-black flex justify-between h-12 ">
             
-            <Switch 
-                checked={toggle} 
-                onChange={(e) => {
-                    setToggle(e.target.checked)
-                    clearCommand()
-                }}
-                color="primary" 
-            />
-
-            <div className='flex justify-center place-items-center text-white gap-1  scale-75 hover:bg-stone-900 p-5 rounded' style={{transition : ".5s"}} onClick={() => copy(getCommand())}>
-                {(isCopied) ? <CheckIcon /> : <ContentCopyIcon /> }
-                <Typography variant="h6" className='font-bold'> Copy </Typography>
+                <div className='flex justify-center place-items-center text-white gap-1  scale-75  p-1 rounded' style={{transition : ".5s"}} >
+                    
+                    <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png"
+                    className='w-10 h-10 overflow-hidden rounded'
+                    />
+                    <Switch 
+                        checked={toggle} 
+                        onChange={(e) => {
+                            setToggle(e.target.checked)
+                            clearCommand()
+                        }}
+                        color="primary" 
+                        className='bg-stone-300 rounded'
+                    />
+                    <img 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png"
+                    className='w-10 h-10 overflow-hidden rounded'
+                    />
+                    
+                </div>
+                    
+                {/*second div*/}
+                <div className='flex justify-end place-items-center text-white gap-1  scale-75 hover:bg-stone-900 p-5 rounded' style={{transition : ".5s"}} onClick={() => copy(getCommand())}>
+                    {(isCopied) ? <CheckIcon /> : <ContentCopyIcon /> }
+                    <Typography variant="h6" className='font-bold'> Copy </Typography>
+                </div>
+            
             </div>
             
-            </div>
             <div className="container-fluid overflow-auto h-12 " style={{backgroundColor : "#1E2022"}}>
             <h1 className="p-3  font-bold text-sm   tracking-wide" style={{color : "#C9D6DF"}}> 
             {
